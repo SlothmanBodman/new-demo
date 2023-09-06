0030df52f90dfe08371830b3dcd0be622d6967e5
Jack Bodman <jack.bodman1998@outlook.com>
2023-09-06 20:23:19 +0100

    Soup Relationship

--- new-demo/app/Models/User.php
+++ new-demo/app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Soup;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function soups(): HasMany
+    {
+        return $this->hasMany(Soup::class);
+    }
 }
