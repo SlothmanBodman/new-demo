8446ea97a8e84ed41dbe901fc2b6d0ffff7913fe
Jack Bodman <jack.bodman1998@outlook.com>
2023-09-16 20:23:18 +0100

    Small db table fix

--- new-demo/database/migrations/2023_09_06_190840_create_soup_table.php
+++ new-demo/database/migrations/2023_09_06_190840_create_soup_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('soup', function (Blueprint $table) {
+        Schema::create('soups', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('soup');
+        Schema::dropIfExists('soups');
     }
 };
