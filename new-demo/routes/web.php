e43c681a786859bdd2b73a1e1e67857fd2b53a05
Jack Bodman <jack.bodman1998@outlook.com>
2023-09-16 20:26:49 +0100

    Call Livewire Componet from route

--- new-demo/routes/web.php
+++ new-demo/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Soup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/soup', Soup::class)->name('soup');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
